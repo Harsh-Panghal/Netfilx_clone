66cf1e83079c3d4d340a32160b9a992b4146dd6f
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-08-24 17:16:34 +0530

    changes via local

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -28,7 +28,7 @@ const Navigation = () => {
     }`}>
       <div className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
-          <div className="text-2xl font-bold bg-gradient-hero bg-clip-text text-transparent">
+          <div className="text-2xl font-bold ">
             Portfolio
           </div>
 
